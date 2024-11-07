622db68eb1028ee4ce2627e6100273bcec05c2f3
Bruno Onofre <bruno.m.onofre@hotmail.com>
2024-11-07 16:25:22 +0000

    Fix botoes

--- add_botas.php
+++ add_botas.php
@@ -25,7 +25,7 @@
                 <label class="btn btn-outline-primary admissao" for="admissao1">Admissão</label>
 
                 <input type="radio" class="btn-check" name="admissao" id="admissao2" autocomplete="off" value="0">
-                <label class="btn btn-outline-primary admissao" for="admissao2">Renovação</label>
+                <label class="btn btn-outline-primary" for="admissao2">Renovação</label>
             </div>
             <div class="input-group">
                 <span class="input-group-text" id="nome">Nome</span>
